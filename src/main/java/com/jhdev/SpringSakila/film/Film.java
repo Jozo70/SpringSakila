940d6630236f8860fcfca2b2acaccfcfe6a97ba3
Joshua Hollick <j.hollick4863@student.leedsbeckett.ac.uk>
2022-07-28 16:49:00 +0100

    Added to Film to include all columns,

--- src/main/java/com/jhdev/SpringSakila/film/Film.java
+++ src/main/java/com/jhdev/SpringSakila/film/Film.java
@@ -20,6 +20,33 @@ public class Film {
     @Column(name = "release_year")
     private int releaseYear;
 
+    @Column(name = "language_id")
+    private int languageId;
+
+    @Column(name = "original_language_id")
+    private int originalLanguageId;
+
+    @Column(name = "rental_duration")
+    private int rentalDuration;
+
+    @Column(name = "rental_rate")
+    private float rentalRate;
+
+    @Column(name = "length")
+    private float length;
+
+    @Column(name = "replacement_cost")
+    private float replacementCost;
+
+    @Column(name = "rating")
+    private float rating;
+
+    @Column(name = "special_features")
+    private float special_features;
+
+    @Column(name = "last_update")
+    private float lastUpdate;
+
     public Film(){}
 
     public int getfilmID() {
@@ -51,5 +78,77 @@ public class Film {
 
     public void setReleaseYear(int releaseYear) {
         this.releaseYear = releaseYear;
+    }
+
+    public int getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(int languageId) {
+        this.languageId = languageId;
+    }
+
+    public int getOriginalLanguageId() {
+        return originalLanguageId;
+    }
+
+    public void setOriginalLanguageId(int originalLanguageId) {
+        this.originalLanguageId = originalLanguageId;
+    }
+
+    public int getRentalDuration() {
+        return rentalDuration;
+    }
+
+    public void setRentalDuration(int rentalDuration) {
+        this.rentalDuration = rentalDuration;
+    }
+
+    public float getRentalRate() {
+        return rentalRate;
+    }
+
+    public void setRentalRate(float rentalRate) {
+        this.rentalRate = rentalRate;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public float getReplacementCost() {
+        return replacementCost;
+    }
+
+    public void setReplacementCost(float replacementCost) {
+        this.replacementCost = replacementCost;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public float getSpecial_features() {
+        return special_features;
+    }
+
+    public void setSpecial_features(float special_features) {
+        this.special_features = special_features;
+    }
+
+    public float getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(float lastUpdate) {
+        this.lastUpdate = lastUpdate;
     }
 }
