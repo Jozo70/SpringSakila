b53107b5fc8c3ee46ecc5837e66372db29d9bca8
Joshua Hollick <j.hollick4863@student.leedsbeckett.ac.uk>
2022-08-25 23:47:47 +0100

    SonarCloud Coverage Test

--- src/test/java/com/jhdev/SpringSakila/Actor/ActorStepDefs.java
+++ src/test/java/com/jhdev/SpringSakila/Actor/ActorStepDefs.java
@@ -1,34 +1,34 @@
-package com.jhdev.SpringSakila.Actor;
-
-import com.jhdev.SpringSakila.actor.Actor;
-import com.jhdev.SpringSakila.actor.ActorDTO;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class ActorStepDefs {
-    private String firstName;
-    private String lastName;
-    private ActorDTO actorDTO;
-    private Actor actor;
-
-    @Given("The Actors Name is Sammy Struggle")
-    public void the_actors_name_is_sammy_struggle(){
-        firstName = "Sammy";
-        lastName = "Struggle";
-    }
-    @When("I Create a new actor called Sammy Struggle")
-    public void i_create_a_new_actor_called_sammy_struggle(){
-        actorDTO = new ActorDTO("Sammy", "Struggle");
-        actor = new Actor(actorDTO);
-    }
-
-    @Then("I should Get Sammy Struggle")
-    public void i_should_get_sammy_struggle(){
-        assertEquals("Sammy", actor.getFirstName(),"The first Name is unexpected");
-        assertEquals("Struggle", actor.getLastName(), "The last name is unexpected");
-    }
-
-}
+//package com.jhdev.SpringSakila.Actor;
+//
+//import com.jhdev.SpringSakila.actor.Actor;
+//import com.jhdev.SpringSakila.actor.ActorDTO;
+//import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.Then;
+//import io.cucumber.java.en.When;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//public class ActorStepDefs {
+//    private String firstName;
+//    private String lastName;
+//    private ActorDTO actorDTO;
+//    private Actor actor;
+//
+//    @Given("The Actors Name is Sammy Struggle")
+//    public void the_actors_name_is_sammy_struggle(){
+//        firstName = "Sammy";
+//        lastName = "Struggle";
+//    }
+//    @When("I Create a new actor called Sammy Struggle")
+//    public void i_create_a_new_actor_called_sammy_struggle(){
+//        actorDTO = new ActorDTO("Sammy", "Struggle");
+//        actor = new Actor(actorDTO);
+//    }
+//
+//    @Then("I should Get Sammy Struggle")
+//    public void i_should_get_sammy_struggle(){
+//        assertEquals("Sammy", actor.getFirstName(),"The first Name is unexpected");
+//        assertEquals("Struggle", actor.getLastName(), "The last name is unexpected");
+//    }
+//
+//}
